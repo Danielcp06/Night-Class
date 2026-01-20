c06ff009b18ae83ad40a8caa6491483bee31514f
alumno <danielchamorropinto1@gmail.com>
2026-01-20 12:50:17 +0100

    clase entidad

---
+++ src/main/java/aventura/app/Entidad.java
@@ -0,0 +1,13 @@
+package aventura.app;
+
+public abstract class Entidad {
+    private String descripcion;
+    private String nombre;
+
+    public Entidad(String descripcion, String nombre) {
+        this.descripcion = descripcion;
+        this.nombre = nombre;
+    }
+
+
+}
