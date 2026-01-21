9f8ffbceedbdc90aeb5b0d2d830c32f40445d08b
alumno <rgomlop37@gmail.com>
2026-01-21 08:37:01 +0100

    clase objeto

--- src/main/java/aventura/app/Objeto.java
+++ src/main/java/aventura/app/Objeto.java
@@ -1,27 +1,4 @@
 package aventura.app;
 
-public abstract class Objeto {
-    protected String nombre;
-    protected String descripcion;
-
-    public Objeto(String nombre, String descripcion) {
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
+public abstract class Objeto extends Entidad{
 }
