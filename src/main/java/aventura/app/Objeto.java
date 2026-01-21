591844b94bc93fc070f15cb3721bb1eccc80401c
alumno <rgomlop37@gmail.com>
2026-01-21 09:01:37 +0100

    clase objeto otra ve

--- src/main/java/aventura/app/Objeto.java
+++ src/main/java/aventura/app/Objeto.java
@@ -1,7 +1,18 @@
 package aventura.app;
 
 public abstract class Objeto extends Entidad{
-    public Objeto(String descripcion, String nombre) {
+    private boolean visible;
+
+    public Objeto(String descripcion, String nombre, boolean visible) {
         super(descripcion, nombre);
+        this.visible = visible;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
     }
 }
