abbde15965053225a837a7de6d8118ef52cbd0ba
alumno <danielchamorropinto1@gmail.com>
2026-01-22 13:15:25 +0100

    clase habitacion

--- src/main/java/aventura/app/Juego.java
+++ src/main/java/aventura/app/Juego.java
@@ -12,7 +12,7 @@ public class Juego {
     // --- NÚCLEO: Definición de Datos (FASE 1) ---
     // Esta parte os la damos HECHA. Es el "contrato" del núcleo.
 
-    private static String descripcionJuego = "Seis meses después del primer día del apocalipsis, eres J.A Bermudo, un exdetective \n" +
+    private static final String descripcionJuego = "Seis meses después del primer día del apocalipsis, eres J.A Bermudo, un exdetective \n" +
             "obsesionado con encontrar el \"Protocolo P.R.C\", la clave del brote, de la Dra. Sonia Joao. Tu pista te \n" +
             "lleva al sellado edificio de la Corporación Miravent, un laberinto silencioso lleno de peligros.\n" +
             "En la recepción, tu objetivo es encontrar información en las distintas salas para avanzar. Te enfrentas \n" +
@@ -21,49 +21,49 @@ public class Juego {
 
     // El mapa de habitaciones.
     // TODO: (Skin) ¡Rellenad esto con vuestras descripciones!
-    private static String[] habitaciones = {
+    private Habitacion[] habitaciones = {
 
 
-                    "SALA DE DESCANSO:Una cafetera queda encendida, burbujeando un café quemado con olor rancio. Sillas volcadas y bandejas con comida a medio comer sugieren una interrupción brusca.\n" +
+                    new Habitacion("SALA DE DESCANSO:Una cafetera queda encendida, burbujeando un café quemado con olor rancio. Sillas volcadas y bandejas con comida a medio comer sugieren una interrupción brusca.\n" +
                             " Hay casilleros abiertos: dentro hay pertenencias personales, fotos de familia y tarjetas de acceso.\n" +
                             " En una mesa, un móvil vibra sin parar, mostrando una notificación repetida:\n" +
-                            "\n" + "“Protocolo interno de emergencia activado. No abandonar el edificio.”",//Posición 0
+                            "\n" + "“Protocolo interno de emergencia activado. No abandonar el edificio.”"), //Posición 0
 
-                    "SALA DE MANTENIMIENTO:Herramientas desparramadas cubren el suelo. Linternas, cinta aislante y un carrito de herramientas bloquean parcialmente una puerta lateral.\n" +
+                    new Habitacion("SALA DE MANTENIMIENTO:Herramientas desparramadas cubren el suelo. Linternas, cinta aislante y un carrito de herramientas bloquean parcialmente una puerta lateral.\n" +
                             " El aire huele a aceite y metal. Un tablero eléctrico parpadea con advertencias rojas:\n" +
                             "\n" + "“Fallo en el suministro de energía — plantas inferiores comprometidas.”\n" +
-                            "Cerca del tablero, hay marcas de arañazos profundas, como si alguien hubiera intentado entrar… o salir", //Posición 1
+                            "Cerca del tablero, hay marcas de arañazos profundas, como si alguien hubiera intentado entrar… o salir"), //Posición 1
 
-                    "LABORATORIO DE INVESTIGACIÓN:la puerta está trabada a medias, dejando un espacio estrecho para entrar. Luces rojas pulsantes bañan la sala. Tubos de ensayo rotos y frascos marcados con símbolos biológicos cubren las mesas. En el fondo, una cámara de contención de vidrio está agrietada desde dentro.\n" +
+                    new Habitacion("LABORATORIO DE INVESTIGACIÓN:la puerta está trabada a medias, dejando un espacio estrecho para entrar. Luces rojas pulsantes bañan la sala. Tubos de ensayo rotos y frascos marcados con símbolos biológicos cubren las mesas. En el fondo, una cámara de contención de vidrio está agrietada desde dentro.\n" +
                             "Un monitor reproduce una grabación detenida en una frase:\n" +
-                            "\n" + "“¡Aún no está listo para la exposición humana!”", //Posición 2
+                            "\n" + "“¡Aún no está listo para la exposición humana!”"), //Posición 2
 
-                    "CENTRO DE CONTROL:pantallas mostrando cámaras del edificio dan la impresión de que alguien aún vigila. En algunas se ven pasillos vacíos, pero en otras se distinguen siluetas moviéndose lento y errático.\n" +
+                    new Habitacion("CENTRO DE CONTROL:pantallas mostrando cámaras del edificio dan la impresión de que alguien aún vigila. En algunas se ven pasillos vacíos, pero en otras se distinguen siluetas moviéndose lento y errático.\n" +
                             "Un panel abierto muestra cables quemados. Sobre el escritorio hay un registro con la última entrada:\n" +
-                            "\n" + "“Bloqueen todos los accesos. No dejen salir nada.”", //Posición 3
+                            "\n" + "“Bloqueen todos los accesos. No dejen salir nada.”"),//Posición 3
 
-                    "RECEPCIÓN:estas en la recepción inicial de la corporación miravent.Un gran mostrador de metal domina la entrada, cubierto de polvo y papeles amarillentos. \n" +
+                    new Habitacion("RECEPCIÓN:estas en la recepción inicial de la corporación miravent.Un gran mostrador de metal domina la entrada, cubierto de polvo y papeles amarillentos. \n" +
                             "El logotipo de la corporación —medio borrado— adorna la pared del fondo, con luces que parpadean débilmente.\n" +
                             "El suelo está lleno de huellas secas y trozos de cristales rotos; una silla caída sugiere que alguien salió con prisa. \n" +
-                            "En una esquina, una planta marchita aún permanece en su maceta, junto a una pantalla que muestra el mensaje: “MANTÉNGASE TRANQUILO. LA SITUACIÓN ESTÁ BAJO CONTROL.\n", // Posición 4
+                            "En una esquina, una planta marchita aún permanece en su maceta, junto a una pantalla que muestra el mensaje: “MANTÉNGASE TRANQUILO. LA SITUACIÓN ESTÁ BAJO CONTROL.\n"), // Posición 4
 
-                    "SALA DE DOCUMENTACIÓN:estantes metálicos hasta el techo, llenos de carpetas con sellos: Clasificado / Confidencial / Proyecto AURORA. El suelo está tapizado de papeles, como si alguien buscara algo con desesperación.\n" +
+                    new Habitacion("SALA DE DOCUMENTACIÓN:estantes metálicos hasta el techo, llenos de carpetas con sellos: Clasificado / Confidencial / Proyecto AURORA. El suelo está tapizado de papeles, como si alguien buscara algo con desesperación.\n" +
                             "Una carpeta abierta en una mesa muestra un gráfico con curvas ascendentes y una nota adherida:\n" +
-                            "\n" + "“La tasa de infección es exponencial. No podemos controlarlo.”",//Posición 5
+                            "\n" + "“La tasa de infección es exponencial. No podemos controlarlo.”"),//Posición 5
 
-                    "LABORATORIO QUÍMICO:olor penetrante, vidrios rotos y líquidos reaccionando todavía sobre una superficie metálica. La campana extractora sigue funcionando, absorbiendo un humo blanco.\n" +
+                    new Habitacion("LABORATORIO QUÍMICO:olor penetrante, vidrios rotos y líquidos reaccionando todavía sobre una superficie metálica. La campana extractora sigue funcionando, absorbiendo un humo blanco.\n" +
                             "En la pizarra hay ecuaciones químicas y una frase escrita con trazos frenéticos:\n" +
-                            "\n" + "“Deja de buscar cura. Evalúa contención.”",//Posición 6
+                            "\n" + "“Deja de buscar cura. Evalúa contención.”"),//Posición 6
 
-                    "SALA DE SERVICIO:la temperatura aquí es extrañamente baja. El zumbido constante de las máquinas genera un ambiente sofocante. Los servidores parpadean como estrellas artificiales.\n" +
+                    new Habitacion("SALA DE SERVICIO:la temperatura aquí es extrañamente baja. El zumbido constante de las máquinas genera un ambiente sofocante. Los servidores parpadean como estrellas artificiales.\n" +
                             "Una terminal desbloqueada muestra un archivo:\n" +
                             "\n" + "“PROTOCOLO FASE FINAL – Autorización Nivel 7 requerida.”\n" +
-                            "La barra para ingresar contraseña está manchada de sangre seca.",//Posición 7
+                            "La barra para ingresar contraseña está manchada de sangre seca."),//Posición 7
 
-                    "ALMACÉN DE PROVISIONES:cajas apiladas por todas partes, muchas con el logo corporativo y la frase Distribución Global. Algunas contienen trajes de bioseguridad, otras jeringas vacías.\n" +
+                    new Habitacion("ALMACÉN DE PROVISIONES:cajas apiladas por todas partes, muchas con el logo corporativo y la frase Distribución Global. Algunas contienen trajes de bioseguridad, otras jeringas vacías.\n" +
                             "Una puerta de carga industrial está atrancada con un carro elevador.\n" +
                             "Sobre una caja abierta hay un mapa del edificio, marcado con un fluorescente:\n" +
-                            "\n" + "“RUTA ESCAPE — SOLO PERSONAL AUTORIZADO”",//Posición 8
+                            "\n" + "“RUTA ESCAPE — SOLO PERSONAL AUTORIZADO”"),//Posición 8
 
     };
 
@@ -104,7 +104,7 @@ public class Juego {
     }
 
 // Variable que guarda la posición actual del jugador
-private static int habitacionActual = 4; // Empezamos en la primera habitación
+private int habitacionActual = 4; // Empezamos en la primera habitación
 
 /**
  * Metodo para ir a la derecha
@@ -113,7 +113,7 @@ public static void derecha() {
     if (habitacionActual == habitaciones.length - 1) {
         System.out.println("No hay mas habitaciones a la derecha. Solo puedes ir a la izquierda");
     } else {
-        System.out.println(habitaciones[habitacionActual + 1]);
+        System.out.println(habitaciones[habitacionActual + 1].getDescripcion());
         habitacionActual = habitacionActual + 1;
     }
 
@@ -126,7 +126,7 @@ public static void izquierda() {
     if (habitacionActual == 0) {
         System.out.println("No hay mas habitaciones a la izquierda. Solo puedes ir a la derecha");
     } else {
-        System.out.println(habitaciones[habitacionActual - 1]);
+        System.out.println(habitaciones[habitacionActual - 1].getDescripcion());
         habitacionActual = habitacionActual - 1;
     }
 }
@@ -207,7 +207,7 @@ private static boolean guardarObjeto(String objeto) {
 
         System.out.println(descripcionJuego);
 
-        System.out.println(habitaciones[habitacionActual]);
+        System.out.println(habitaciones[habitacionActual].getDescripcion());
 
         System.out.println("Las opciones son ayuda, mirar, inventario, \n" +
                 "ir derecha, ir izquierda, coger [objeto] y salir");
