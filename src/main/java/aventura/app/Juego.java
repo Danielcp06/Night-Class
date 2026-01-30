deabdb84edc47c191ca8e24ec0f95c1bfa8f19eb
alumno <danielchamorropinto1@gmail.com>
2026-01-30 08:38:42 +0100

    clase habitacion

--- src/main/java/aventura/app/Juego.java
+++ src/main/java/aventura/app/Juego.java
@@ -9,75 +9,48 @@ import java.util.Scanner;
  */
 public class Juego {
 
-    // --- NÚCLEO: Definición de Datos (FASE 1) ---
-    // Esta parte os la damos HECHA. Es el "contrato" del núcleo.
-
-    private static final String descripcionJuego = "Seis meses después del primer día del apocalipsis, eres J.A Bermudo, un exdetective \n" +
-            "obsesionado con encontrar el \"Protocolo P.R.C\", la clave del brote, de la Dra. Sonia Joao. Tu pista te \n" +
-            "lleva al sellado edificio de la Corporación Miravent, un laberinto silencioso lleno de peligros.\n" +
-            "En la recepción, tu objetivo es encontrar información en las distintas salas para avanzar. Te enfrentas \n" +
-            "a tu primera elección crucial: sala del servicio de mantenimiento (Izquierda) o las escaleras de servicio (Derecha) \n" +
-            "hacia niveles inferiores. Tu supervivencia depende de la información que encuentres en cada sala. \n";
-
-    // El mapa de habitaciones.
-    // TODO: (Skin) ¡Rellenad esto con vuestras descripciones!
-    private Habitacion[] habitaciones = {
+    private Jugador j;
+    private int habitacionActual;//Habitación en la que empezaremos el juego
+    private Habitacion[] habitaciones;
+    private String descripcionJuego;
 
 
-                    new Habitacion("SALA DE DESCANSO:Una cafetera queda encendida, burbujeando un café quemado con olor rancio. Sillas volcadas y bandejas con comida a medio comer sugieren una interrupción brusca.\n" +
-                            " Hay casilleros abiertos: dentro hay pertenencias personales, fotos de familia y tarjetas de acceso.\n" +
-                            " En una mesa, un móvil vibra sin parar, mostrando una notificación repetida:\n" +
-                            "\n" + "“Protocolo interno de emergencia activado. No abandonar el edificio.”"), //Posición 0
+    public Juego(Jugador j, int habitacionActual) {
+        this.j = j;
+        this.habitacionActual = habitacionActual;
+        this.habitaciones = new Habitacion[3];
+    }
 
-                    new Habitacion("SALA DE MANTENIMIENTO:Herramientas desparramadas cubren el suelo. Linternas, cinta aislante y un carrito de herramientas bloquean parcialmente una puerta lateral.\n" +
-                            " El aire huele a aceite y metal. Un tablero eléctrico parpadea con advertencias rojas:\n" +
-                            "\n" + "“Fallo en el suministro de energía — plantas inferiores comprometidas.”\n" +
-                            "Cerca del tablero, hay marcas de arañazos profundas, como si alguien hubiera intentado entrar… o salir"), //Posición 1
+    private void inicializar() {
+        descripcionJuego = "Seis meses después del primer día del apocalipsis, eres J.A Bermudo, un exdetective \n" +
+                "obsesionado con encontrar el \"Protocolo P.R.C\", la clave del brote, de la Dra. Sonia Joao. Tu pista te \n" +
+                "lleva al sellado edificio de la Corporación Miravent, un laberinto silencioso lleno de peligros.\n" +
+                "En la recepción, tu objetivo es encontrar información en las distintas salas para avanzar. Te enfrentas \n" +
+                "a tu primera elección crucial: sala del servicio de mantenimiento (Izquierda) o las escaleras de servicio (Derecha) \n" +
+                "hacia niveles inferiores. Tu supervivencia depende de la información que encuentres en cada sala. \n";
 
-                    new Habitacion("LABORATORIO DE INVESTIGACIÓN:la puerta está trabada a medias, dejando un espacio estrecho para entrar. Luces rojas pulsantes bañan la sala. Tubos de ensayo rotos y frascos marcados con símbolos biológicos cubren las mesas. En el fondo, una cámara de contención de vidrio está agrietada desde dentro.\n" +
-                            "Un monitor reproduce una grabación detenida en una frase:\n" +
-                            "\n" + "“¡Aún no está listo para la exposición humana!”"), //Posición 2
+        Habitacion h0 = new Habitacion("SALA DE DESCANSO:Una cafetera queda encendida, burbujeando un café quemado con olor rancio. Sillas volcadas y bandejas con comida a medio comer sugieren una interrupción brusca.\n" +
+                " Hay casilleros abiertos: dentro hay pertenencias personales, fotos de familia y tarjetas de acceso.\n" +
+                " En una mesa, un móvil vibra sin parar, mostrando una notificación repetida:\n" +
+                "\n" + "“Protocolo interno de emergencia activado. No abandonar el edificio.”");
 
-                    new Habitacion("CENTRO DE CONTROL:pantallas mostrando cámaras del edificio dan la impresión de que alguien aún vigila. En algunas se ven pasillos vacíos, pero en otras se distinguen siluetas moviéndose lento y errático.\n" +
-                            "Un panel abierto muestra cables quemados. Sobre el escritorio hay un registro con la última entrada:\n" +
-                            "\n" + "“Bloqueen todos los accesos. No dejen salir nada.”"),//Posición 3
-
-                    new Habitacion("RECEPCIÓN:estas en la recepción inicial de la corporación miravent.Un gran mostrador de metal domina la entrada, cubierto de polvo y papeles amarillentos. \n" +
-                            "El logotipo de la corporación —medio borrado— adorna la pared del fondo, con luces que parpadean débilmente.\n" +
-                            "El suelo está lleno de huellas secas y trozos de cristales rotos; una silla caída sugiere que alguien salió con prisa. \n" +
-                            "En una esquina, una planta marchita aún permanece en su maceta, junto a una pantalla que muestra el mensaje: “MANTÉNGASE TRANQUILO. LA SITUACIÓN ESTÁ BAJO CONTROL.\n"), // Posición 4
-
-                    new Habitacion("SALA DE DOCUMENTACIÓN:estantes metálicos hasta el techo, llenos de carpetas con sellos: Clasificado / Confidencial / Proyecto AURORA. El suelo está tapizado de papeles, como si alguien buscara algo con desesperación.\n" +
-                            "Una carpeta abierta en una mesa muestra un gráfico con curvas ascendentes y una nota adherida:\n" +
-                            "\n" + "“La tasa de infección es exponencial. No podemos controlarlo.”"),//Posición 5
-
-                    new Habitacion("LABORATORIO QUÍMICO:olor penetrante, vidrios rotos y líquidos reaccionando todavía sobre una superficie metálica. La campana extractora sigue funcionando, absorbiendo un humo blanco.\n" +
-                            "En la pizarra hay ecuaciones químicas y una frase escrita con trazos frenéticos:\n" +
-                            "\n" + "“Deja de buscar cura. Evalúa contención.”"),//Posición 6
-
-                    new Habitacion("SALA DE SERVICIO:la temperatura aquí es extrañamente baja. El zumbido constante de las máquinas genera un ambiente sofocante. Los servidores parpadean como estrellas artificiales.\n" +
-                            "Una terminal desbloqueada muestra un archivo:\n" +
-                            "\n" + "“PROTOCOLO FASE FINAL – Autorización Nivel 7 requerida.”\n" +
-                            "La barra para ingresar contraseña está manchada de sangre seca."),//Posición 7
-
-                    new Habitacion("ALMACÉN DE PROVISIONES:cajas apiladas por todas partes, muchas con el logo corporativo y la frase Distribución Global. Algunas contienen trajes de bioseguridad, otras jeringas vacías.\n" +
-                            "Una puerta de carga industrial está atrancada con un carro elevador.\n" +
-                            "Sobre una caja abierta hay un mapa del edificio, marcado con un fluorescente:\n" +
-                            "\n" + "“RUTA ESCAPE — SOLO PERSONAL AUTORIZADO”"),//Posición 8
-
-    };
+        habitaciones[0] = h0;
+        Habitacion h1 = new Habitacion("RECEPCIÓN:estas en la recepción inicial de la corporación miravent.Un gran mostrador de metal domina la entrada, cubierto de polvo y papeles amarillentos. \n" +
+                "El logotipo de la corporación —medio borrado— adorna la pared del fondo, con luces que parpadean débilmente.\n" +
+                "El suelo está lleno de huellas secas y trozos de cristales rotos; una silla caída sugiere que alguien salió con prisa. \n" +
+                "En una esquina, una planta marchita aún permanece en su maceta, junto a una pantalla que muestra el mensaje: “MANTÉNGASE TRANQUILO. LA SITUACIÓN ESTÁ BAJO CONTROL.\n");
+        habitaciones[1] = h1;
+        Habitacion h2 = new Habitacion("LABORATORIO DE INVESTIGACIÓN:la puerta está trabada a medias, dejando un espacio estrecho para entrar. Luces rojas pulsantes bañan la sala. Tubos de ensayo rotos y frascos marcados con símbolos biológicos cubren las mesas. En el fondo, una cámara de contención de vidrio está agrietada desde dentro.\n" +
+                "Un monitor reproduce una grabación detenida en una frase:\n" +
+                "\n" + "“¡Aún no está listo para la exposición humana!”");
+        habitaciones[2] = h2;
+    }
 
     // Los objetos que hay en cada habitación.
     public static String[][] objetosMapa = {
             {"llave inglesa", null},           // Objetos en Habitación 0
             {null, null},           // Objetos en Habitación 1
             {"taza", null},         // Objetos en Habitación 2
-            {"radio", "tarjeta de acceso"},           // Objetos en Habitación 3
-            {null, null},           // Objetos en Habitación 4
-            {null, null},           // Objetos en habitación 5
-            {"jeringuilla", "probeta"},           // Objetos en Habitación 6
-            {null, null},           // Objetos en Habitación 7
-            {"alubias en lata", null},           // Objetos en Habitación 8
 
     };
 
@@ -103,13 +76,11 @@ public class Juego {
         }
     }
 
-// Variable que guarda la posición actual del jugador
-private int habitacionActual = 4; // Empezamos en la primera habitación
 
 /**
  * Metodo para ir a la derecha
  */
-public static void derecha() {
+public void derecha() {
     if (habitacionActual == habitaciones.length - 1) {
         System.out.println("No hay mas habitaciones a la derecha. Solo puedes ir a la izquierda");
     } else {
@@ -122,7 +93,7 @@ public static void derecha() {
 /**
  * Metodo para ir a la izquierda
  */
-public static void izquierda() {
+public void izquierda() {
     if (habitacionActual == 0) {
         System.out.println("No hay mas habitaciones a la izquierda. Solo puedes ir a la derecha");
     } else {
@@ -134,7 +105,7 @@ public static void izquierda() {
 /*+
  * Metodo para coger objetos
  */
-public static void cogerObjetos() {
+public void cogerObjetos() {
     Scanner sc = new Scanner(System.in);
     //llamar a listar objetos
     int numeroObjetos = listarObjetos();
@@ -166,7 +137,7 @@ public static void cogerObjetos() {
 
 }
 
-private static int listarObjetos() {
+private int listarObjetos() {
     int contador = 0;
     for (int i = 0; i < objetosMapa[habitacionActual].length; i++) {
         if (objetosMapa[habitacionActual][i] != null) {
@@ -178,7 +149,7 @@ private static int listarObjetos() {
     return contador;
 }
 
-private static boolean guardarObjeto(String objeto) {
+private boolean guardarObjeto(String objeto) {
     int ocupado = 0;
     for (int i = 0; i < inventario.length; i++) {
         if (inventario[i] != null) ocupado++;
@@ -205,6 +176,8 @@ private static boolean guardarObjeto(String objeto) {
         System.out.println("'LA CURA'");
         System.out.println("------------------------------------------");
 
+       Juego t = new Juego("Ruben", 4);
+
         System.out.println(descripcionJuego);
 
         System.out.println(habitaciones[habitacionActual].getDescripcion());
@@ -227,14 +200,14 @@ private static boolean guardarObjeto(String objeto) {
              */
         switch (comando.toLowerCase()) {
             case "ir derecha":
-                derecha();
+                t.derecha();
                 break;
             case "ir izquierda":
-                izquierda();
+                t.izquierda();
                 break;
             case "mirar":
                 System.out.println(habitaciones[habitacionActual]);
-                listarObjetos();
+                t.listarObjetos();
                 break;
             case "salir":
                 jugando = false;
@@ -244,7 +217,7 @@ private static boolean guardarObjeto(String objeto) {
                         "ir derecha, ir izquierda, coger [objeto] y salir \n");
                 break;
             case "coger":
-                cogerObjetos();
+                t.cogerObjetos();
                 break;
             case "inventario":
                 inventarioActual();
